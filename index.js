5809529c557e73fe26da5fba791304897fbe217d
이름 <qkrwnghd512@gmail.com>
2020-12-02 18:29:15 +0900

    ver3

--- index.js
+++ index.js
@@ -23,6 +23,28 @@ client.on('message', (message) => {
     return message.reply('pong');
   }
 
+  if(message.content == '!help') {
+    let img = 'https://lh3.googleusercontent.com/proxy/_vnQDg9FoFsMoCUXl974zdRQXczi6uOVlcS4hrXhW6vrk2Oojqf1k-WpZhmwlcQ9NkFBu-EdlabOhT1U7789wk7uYbed6p87qsbTmFDUhSdbJKMTRB6XucNMq00Be2LO4ljr42DPpuRla9C4n51uG7FbMJjxxiPoop7x';
+    let embed = new Discord.RichEmbed()
+      .setTitle('헬프')
+      .setURL('http://localhost:3000/la1')
+      .setAuthor('앙 기모띠', img, 'http://localhost:3000/la1')
+      .setThumbnail(img)
+      .addBlankField()
+      .addField('크롤링', '데이따')
+      .addField('Inline field title', '크', true)
+      .addField('Inline field title', '롤', true)
+      .addField('Inline field title', '링', true)
+      .addField('Inline field title', '크\n크\n크\n')
+      .addBlankField()
+      .setTimestamp()
+      .setFooter('앙 기모링~', img)
+      
+
+    message.channel.send(embed)
+  }
+  
+
   if(message.content == '천호') {
     let img = 'https://lh3.googleusercontent.com/proxy/_vnQDg9FoFsMoCUXl974zdRQXczi6uOVlcS4hrXhW6vrk2Oojqf1k-WpZhmwlcQ9NkFBu-EdlabOhT1U7789wk7uYbed6p87qsbTmFDUhSdbJKMTRB6XucNMq00Be2LO4ljr42DPpuRla9C4n51uG7FbMJjxxiPoop7x';
     let embed = new Discord.RichEmbed()
@@ -44,31 +66,7 @@ client.on('message', (message) => {
     message.channel.send(embed)
   }
   
-  if(message.content == 'help') {
-    let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
-    let commandList = [
-      {name: 'ping', desc: '현재 핑 상태'},
-      {name: 'embed', desc: 'embed 예제1'},
-      {name: 'embed2', desc: 'embed 예제2 (help)'},
-      {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
-      {name: '!청소', desc: '텍스트 지움'},
-    ];
-    let commandStr = '';
-    let embed = new Discord.RichEmbed()
-      .setAuthor('Help of 콜라곰 BOT', helpImg)
-      .setColor('#186de6')
-      .setFooter(`콜라곰 BOT ❤️`)
-      .setTimestamp()
-    
-    commandList.forEach(x => {
-      commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
-    });
-
-    embed.addField('Commands: ', commandStr);
-
-    message.channel.send(embed)
-  }
-
+  
 
 });
 
